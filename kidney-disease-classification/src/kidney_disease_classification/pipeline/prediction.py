3062d88085ae14399b9d0257cd7f1c84373be2ab
Satvik Sharma <sharmasatvik031@gmail.com>
2026-01-24 13:37:48 +0530

    GitIgnore updated

--- kidney-disease-classification/src/kidney_disease_classification/pipeline/prediction.py
+++ kidney-disease-classification/src/kidney_disease_classification/pipeline/prediction.py
@@ -2,14 +2,23 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from pathlib import Path
+import streamlit as st
+
+
+@st.cache_resource
+def load_model():
+    model_path = Path(__file__).resolve().parents[3] / "model" / "kidney_model.h5"
+
+    if not model_path.exists():
+        raise FileNotFoundError(f"Model not found at {model_path}")
+
+    return tf.keras.models.load_model(model_path)
 
 
 class PredictionPipeline:
     def __init__(self, image_path: str):
         self.image_path = image_path
-
-        model_path = Path(__file__).resolve().parents[3] / "model" / "kidney_model.h5"
-        self.model = tf.keras.models.load_model(model_path)
+        self.model = load_model()
 
     def predict(self):
         img = image.load_img(self.image_path, target_size=(224, 224))
