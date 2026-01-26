29f47f517195df989afcecdfc026e700ff523958
Satvik Sharma <sharmasatvik031@gmail.com>
2026-01-27 01:54:29 +0530

    Fix dependencies: Streamlit + NLP + Visualization stable

--- resume-anylizer/nlp_modules/spacy_loader.py
+++ resume-anylizer/nlp_modules/spacy_loader.py
@@ -1,11 +1,4 @@
 import spacy
-import subprocess
-import sys
-from spacy.util import is_package
 
 def load_spacy_model(model_name="en_core_web_sm"):
-    if not is_package(model_name):
-        subprocess.check_call(
-            [sys.executable, "-m", "spacy", "download", model_name]
-        )
     return spacy.load(model_name)
