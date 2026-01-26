c086f83db7ce9e8a075250028956b971e406fbcc
Satvik Sharma <sharmasatvik031@gmail.com>
2026-01-27 02:11:43 +0530

    fixed requirements

--- resume-anylizer/nlp_modules/spacy_loader.py
+++ resume-anylizer/nlp_modules/spacy_loader.py
@@ -1,4 +1,6 @@
 import spacy
+import os
 
-def load_spacy_model(model_name="en_core_web_sm"):
-    return spacy.load(model_name)
+def load_spacy_model():
+    model_path = os.path.join("spacy_models", "en_core_web_sm")
+    return spacy.load(model_path)
